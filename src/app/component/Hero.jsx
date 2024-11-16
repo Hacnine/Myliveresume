d3271d809a06fc2b75977369e5a90e643b554e2a
Hasnain <hacninetusar@gmail.com>
2024-11-16 21:07:24 +0600

    image size is decreased and hero section's name portion is hide unhide

--- src/app/component/Hero.jsx
+++ src/app/component/Hero.jsx
@@ -14,13 +14,13 @@ const Hero = forwardRef(({ scrollToRef }, ref) => {
   }
 
   return (
-    <div className="flex flex-col container mx-auto md:flex-row items-center justify-center h-full px-6" ref={ref}>
+    <div className="flex flex-col md:mt-0 mt-20 container mx-auto md:flex-row items-center justify-center h-full px-6" ref={ref}>
       {/* Left Section */}
       <motion.div
         initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1 }}
-        className="flex flex-col items-start space-y-4 md:w-1/2"
+        className="md:flex hidden flex-col items-start space-y-4 md:w-1/2"
       >
         <h1 className="text-[55px] font-bold font-poppins leading-tight">
           Hasnain
@@ -60,6 +60,47 @@ const Hero = forwardRef(({ scrollToRef }, ref) => {
       <img src="/images/profile.svg" className="w-[500px]" alt="Profile Illustration" />
 
       {/* Right Section */}
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="flex mt-7 flex-col items-start space-y-4 md:w-1/2 md:hidden "
+      >
+        <h1 className="text-[55px] font-bold font-poppins leading-tight">
+          Hasnain
+          <span className="block -mt-3">
+            Tushar<span className="text-yellow-500">.</span>
+          </span>
+          <div className="w-6 ml-2.5 bg-yellow-500 h-0.5"></div>
+        </h1>
+
+        <p className="text-yellow-500 font-roboto">
+          <a
+            href="https://github.com/hacnine"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            GitHub
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://www.linkedin.com/in/hacnine/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            LinkedIn
+          </a>
+        </p>
+        <a
+          href="https://drive.google.com/file/d/1-s4V3DwRW0c4J2PFn4sBVBIj3O1WuVfn/view?usp=sharing"
+          className="mt-4 px-6 py-3 border-2 border-yellow-500 hover:text-white text-xs font-semibold text-yellow-500 uppercase font-helvetica flex items-center gap-3 hover:bg-yellow-500 transition duration-300 rounded-md"
+        >
+          Download My CV <FaDownload className="text-lg" />
+        </a>
+      </motion.div>
+
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
